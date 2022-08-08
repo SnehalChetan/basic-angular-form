b3fda568cf677a26b51ca02068b0694c0a513adf
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-08-08 12:07:35 +0200

    declare and define AddFriendService in constructor and using this service on form submit send data to friend array of server

--- friendBook/src/app/app.component.ts
+++ friendBook/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Friend } from './friend';
-
+import { AddFriendService } from './add-friend.service';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,31 @@ import { Friend } from './friend';
 })
 export class AppComponent {
   title = 'friendBook';
-  languages: string[];
-
+  languages: string[] = [
+      "html",
+      "css",
+      "js",
+      "php",
+      "phython",
+      "django"
+    ];
+  /**
+   * create a new friend
+   */
   myfriend = new Friend("","","","",0,"");
-    constructor() {
 
-      this.languages = [
-        "html",
-        "css",
-        "js",
-        "php",
-        "phython",
-        "django"
-      ];
+  /**
+   * constructor for language dropdown
+   */
+
+  constructor(private _addFriendService: AddFriendService) {}
+  /**
+   * method to submit a form with validation
+   */
+   submitFriendForm () {
+    //console.log(this.myfriend);
+    let observables = this._addFriendService.addFriend(this.myfriend);
+    //console.log(observables);
+    observables.subscribe(data=>console.log(data), error=>console.log(error));
   }
 }
-
